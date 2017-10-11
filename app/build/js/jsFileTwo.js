a0ef41f36be361cc8f6b6d6ea5b430fb3d2eb490
STAFF\zhangwei36 <1004695936@qq.com>
2017-10-11 11:36:00 +0800

    删除build目录

--- app/build/js/jsFileTwo.js
+++
@@ -1,2 +0,0 @@
-/* @update: 2017-10-11 11:21:21 */ 
-define("project/jsFileTwo",function(require,exports,module){var e=require("project/jsFileThree");module.exports=function(){console.log("jsFileTwo.js"),e()}});
--- app/build/js/main.js
+++
@@ -1,2 +0,0 @@
-/* @update: 2017-10-11 11:21:21 */ 
-seajs.config({comboExcludes:/.*/,paths:{project:"../js/"}}),seajs.use(["project/jsFileOne","project/jsFileTwo"],function(e,s){e(),s()});
