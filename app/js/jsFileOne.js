b45507ef0721035bc7b65bc0a5fea3a594da8eff
STAFF\zhangwei36 <1004695936@qq.com>
2018-02-01 15:37:37 +0800

    移除 js 文件夹下的 js 文件，模板中的 js 引用也去掉，新建唯一主入口main.js

--- app/js/jsFileOne.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zhangwei36 on 2017/10/10.
- */
-
-define("project/jsFileOne", function(require, exports, module) {
-    module.exports = function jsFileOne() {
-        console.log('jsFileOne.js')
-    }
-})
--- app/js/jsFileThree.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by zhangwei36 on 2017/10/10.
- */
-define("project/jsFileThree", function(require, exports, module) {
-    module.exports = function jsFileThree() {
-        console.log('jsFileThree.js')
-    }
-})
